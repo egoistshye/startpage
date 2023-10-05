fce96f7412b98cd14f2a59b7154d2da6a2882dcc
Alicja Kanafa <82821347+egoistshye@users.noreply.github.com>
2023-10-05 09:27:06 +0100

    Update shortcuts.js 051023

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -5,7 +5,6 @@ export default [
     items: {
       Github: "https://github.com",
       AWS: "https://aws.amazon.com",
-      "Dev.to": "https://dev.to",
     },
   },
   {
@@ -15,6 +14,7 @@ export default [
       Outlook: "https://outlook.office365.com/mail/inbox",
       Canva: "https://www.canva.com/en_gb/",
       Recruit: "https://recruit.ulster.ac.uk/",
+      Portal: "https://portal.ulster.ac.uk/",
     },
   },
   {
@@ -33,6 +33,7 @@ export default [
       Amazon: "https://www.amazon.co.uk",
       YouTube: "https://www.youtube.com/",
       Facebook: "https://www.facebook.com/",
+      Twitter: "https://twitter.com/home/",
     },
   },
 ];
